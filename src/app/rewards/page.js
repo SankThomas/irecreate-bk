5fd62a0fc1b3ae80f4a45ef0e2fdffefb8fa5a31
Thomas Sankara <thomassankara74@gmail.com>
2023-07-21 14:34:59 +0300

    rewards page

---
+++ src/app/rewards/page.js
@@ -0,0 +1,12 @@
+import Header from "@/components/home/header";
+import Hero from "@/components/rewards/hero";
+import React from "react";
+
+export default function Rewards() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
