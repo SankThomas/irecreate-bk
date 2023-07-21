f8df63e10d03b3292325a6d440dffe105fb24e2c
Thomas Sankara <thomassankara74@gmail.com>
2023-07-21 14:34:44 +0300

    menu page

---
+++ src/app/menu/page.js
@@ -0,0 +1,12 @@
+import Header from "@/components/menu/header";
+import Hero from "@/components/menu/hero";
+import React from "react";
+
+export default function Menu() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
