6f7d9a5a81bb151726545120e8f3597e8855fdf5
Thomas Sankara <thomassankara74@gmail.com>
2023-07-21 14:34:51 +0300

    offers page

---
+++ src/app/offers/page.js
@@ -0,0 +1,12 @@
+import Header from "@/components/home/header";
+import Foods from "@/components/offers/foods";
+import React from "react";
+
+export default function Offers() {
+  return (
+    <>
+      <Header />
+      <Foods />
+    </>
+  );
+}
